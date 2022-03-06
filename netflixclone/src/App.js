42ce1eaec7152f568856caf87cbe43014b031c10
Edilonab <90010997+Edilonab@users.noreply.github.com>
2022-03-06 00:14:01 -0300

    Organização da pagina

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-    return(
-        <div>
-            Olá Mundo!!!
+
+    const [movieList, setMovieList] = useState([]);
+
+
+    useEffect(()=>{
+        const loadAll = async() => {
+            //pegando a lista ToTAL
+            let list = await Tmdb.getHomeList();
+            console.log(list);
+        }
+
+        loadAll();
+    }, []);
+
+    return (
+        <div className="page">
+            <section className="lists">
+                {movieList.map((item, key)=>(
+                    <MovieRow Key={key} title={item.title} items={item.items} />
+                ))}
+            </section>
         </div>
     );
 }
