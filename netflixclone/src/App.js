780b602e3b30c666a4f82609f71349563e69ef3e
Edilonab <90010997+Edilonab@users.noreply.github.com>
2022-03-26 05:29:56 -0300

    header pronto

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
     const [featuredData, setFeaturedData] = useState(null);
+    const [blackHearder, setBlackHeader] = useState(false);
 
     useEffect(()=>{
         const loadAll = async() => {
@@ -29,8 +31,24 @@ export default () => {
        loadAll();
     }, []);
 
+    useEffect(()=>{
+        const scrollListener =() => {
+            if(window.scrollY > 10) {
+                setBlackHeader(true);
+            } else {
+                setBlackHeader(false);
+            }
+        }
+      window.addEventListener('scroll', scrollListener);
+      return () => {
+        window.removeEventListener('scroll', scrollListener);
+      }
+    }, []);
+    
     return (
         <div className="page">
+
+            <Header black={blackHearder} />
 
             {featuredData &&
              <FeaturedMovie item={featuredData} />
